52d04507e5555d7ec7bfa31825fdf5bf8a48287e
Asmail <asmil9069@gmail.com>
2026-02-09 00:56:27 +0300

    Commited

--- src/index.js
+++ src/index.js
@@ -6,12 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import { ContextProvider } from './Mycontext/APIcontext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <ContextProvider>
        <App />
     </ContextProvider>
    
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
